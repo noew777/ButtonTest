e39041351c0f025a15b00c7972be044ba44b5853
Luis Molina-Tanco <lmtanco@users.noreply.github.com>
2021-11-10 12:01:00 +0100

    Original code by Edouard Renard
    This is the original code by Edouard Renard explained in his tutorial on Arduino and OOP: https://roboticsbackend.com/arduino-object-oriented-programming-oop

---
+++ Button.h
@@ -0,0 +1,30 @@
+// Button.h
+//
+// Original code by Edouard Renard
+// https://roboticsbackend.com/arduino-object-oriented-programming-oop 
+
+#ifndef MY_BUTTON_H
+#define MY_BUTTON_H
+
+#include <Arduino.h>
+
+class Button {
+  
+  private:
+    byte pin;
+    byte state;
+    byte lastReading;
+    unsigned long lastDebounceTime = 0;
+    unsigned long debounceDelay = 50;
+    
+  public:
+    Button(byte pin);
+
+    void init();
+    void update();
+
+    byte getState();
+    bool isPressed();
+};
+
+#endif
